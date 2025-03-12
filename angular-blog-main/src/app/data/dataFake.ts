3ddb501b2ca8c63f67a0189a271201c6ff80a1a0
Melissa <melissavalentindev@gmail.com>
2025-03-12 16:44:39 -0300

    components creteds with styles

---
+++ angular-blog-main/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Nome para Border Collie: veja inspirações para nomear o pet!",
+    "description": "Considerado um dos cães mais inteligentes do mundo, o Border Collie também é conhecido por ser muito carinhoso, ganhando popularidade nos últimos tempos. Se você acabou de adotar um animal dessa raça, é importante escolher um nome para Border Collie que combine com ele. O nome para Border Collie deve ser fácil de entender e memorizar. Palavras curtas são mais tranquilas de serem reconhecidas pelo animal, evitando confusões com outros sons. Além disso, é importante evitar opções semelhantes a comandos ou nomes de familiares, pois isso pode confundir o pet.",
+    "photoCover":"https://www.petz.com.br/blog/wp-content/uploads/2023/07/porque-border-collie-solta-muito-pelo3.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Cachorro gosta do cheiro de citronela? Descubra!",
+    "description": "Não, de maneira geral, cães não gostam do cheiro de citronela. O aroma é forte e pode ser desagradável e muito intenso para os pets. Isso pode até provocar reações como espirros, afastamento do local com o odor e coceiras. Alguns tutores até utilizam a citronela como forma de adestrar os cães, espirrando o produto em locais que o pet não deve fazer xixi.",
+    "photoCover":"https://www.petz.com.br/blog/wp-content/uploads/2025/03/cachorro-citronela1.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Conheça 5 raças de cachorros egípcios!",
+    "description": "Indícios históricos mostram que os cachorros egípcios estão entre os mais antigos do mundo. Alguns desses animais mantêm as características originais até os dias atuais, enquanto outros deram origem a diversas raças. Cão do Faraó, Basenji, Cão Pastor Egípcio de Armant, Saluki e Baladi são as raças de cães egípcios mais antigos.",
+    "photoCover":"https://www.petz.com.br/blog/wp-content/uploads/2024/12/pacing1.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Qual a idade de gato para humano?",
+    "description": "Segundo os estudos, o primeiro ano de vida do gato equivale a 16,6 anos humanos. Depois é que entra a fórmula. A partir de um ano e meio, a conta passa a ser assim: (4,1364 x idade do gato) + 15. Para exemplificar, 4 anos do gato correspondem à idade humana de 31,5 anos (4 x 4,1364 = 16,5 + 15 = 31,5). Agora ficou mais fácil de entender, não é mesmo? Então, corra e descubra quantos anos o seu gato tem!",
+    "photoCover":"https://www.petz.com.br/blog/wp-content/uploads/2019/07/anemia-em-gatos-topo.jpg"
+  }
+]
